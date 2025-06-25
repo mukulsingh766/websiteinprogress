b3381007f0b7db59e72e7380efa63f1ef66bc42f
mukul singh <mukultechwith641@gmail.com>
2025-06-25 10:37:36 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+var a=document.querySelector("#old");
+
+a.addEventListener("click",function(){
+    a.innerHTML="Victory is in having done your best."
+    
+});
+
+ var b=document.querySelector("#second");
+ 
+ setTimeout(function(){
+ b.innerHTML="never give up :-"
+ },5000)
